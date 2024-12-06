f501c0ff5663cf613d1e1e8dfd11aa8f91d2f781
Amber <amber.quynh.nguyen@gmail.com>
2024-12-06 20:29:11 +0800

    css homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,18 +2,33 @@ import useProjects from "../hooks/use-projects";
 import ProjectCard from "../components/ProjectCard";
 import "./HomePage.css";
 import banner from "../assets/banner.jpg";
+import { Typewriter } from "react-simple-typewriter";
 function HomePage() {
   const { projects } = useProjects();
 
   return (
     <div>
       <div className="banner">
-        <img src={banner} />
+        <img
+          src={banner}
+          alt="A background image of a space with rassy mounds, wildflowers, skylights, and modern furniture."
+        />
+        <div className="banner-text">
+          <h1>
+            <Typewriter words={["Welcome to Sprout"]} typeSpeed={100} />
+          </h1>
+          <p>
+            A crowdfunding platform for your next community conservation project
+          </p>
+        </div>
       </div>
-      <div id="project-list">
-        {projects.map((projectData, key) => {
-          return <ProjectCard key={key} projectData={projectData} />;
-        })}
+      <div className="project-page">
+        <h1>Featured projects</h1>
+        <div id="project-list">
+          {projects.map((projectData, key) => {
+            return <ProjectCard key={key} projectData={projectData} />;
+          })}
+        </div>
       </div>
     </div>
   );
