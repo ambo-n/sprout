adc8b8edb51729bf10eb07fd489dbaadbf264538
Amber <amber.quynh.nguyen@gmail.com>
2024-12-08 17:18:50 +0800

    trying to make a 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import ProjectPage from "./pages/ProjectPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import UserPage from "./pages/UserPage.jsx";
 import NewProject from "./pages/NewProject.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 
 import NavBar from "./components/NavBar.jsx";
 import { AuthProvider } from "./components/AuthProvider.jsx";
@@ -21,6 +22,7 @@ const router = createBrowserRouter([
       { path: "/users", element: <UserPage /> },
       { path: "/projects", element: <NewProject /> },
       { path: "/project/:id", element: <ProjectPage /> },
+      { path: "/*", element: <PageNotFound /> },
     ],
   },
 ]);
